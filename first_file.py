a194b020b739f5551f4b3dd755a14c8932f7d592
nina0-887 <142182407+nina0-887@users.noreply.github.com>
2023-08-14 01:19:06 +0300

    Add files via upload

---
+++ first_file.py
@@ -0,0 +1,7 @@
+# Мой первый код на Python
+print('Hello cats 1!')
+print('Hello cats 2!')
+print('Hello cats 3!')
+
+# Это код работает как надо!
+# Автор Нина Багинская
---
+++ second_file.py
@@ -0,0 +1,9 @@
+# Тут будет мой второй код на Python
+print('Hello dogs 1')
+print('Hello dogs 2')
+print('Hello dogs 3')
+print('Hello dogs 4')
+prin('Hello dogs 5')
+
+# Этот код работает справно!
+# Автор -разработчик № 2
